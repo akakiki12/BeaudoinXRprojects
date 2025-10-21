2ebfdd4bc9fa41a850b97de93ab1350e72a351ad
akakiki12 <abeaudoin.pro@gmail.com>
2025-10-21 10:47:57 -0400

    door + lock + core feature scripting

---
+++ XRI_project/Assets/Scripts/CoreFeatures.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.XR;
+using System;
+
+public enum FeatureUsage
+{
+    Once, //use once
+    Toggle, //use on or off
+
+}
+public class CoreFeatures : MonoBehaviour
+{
+    /*
+     * Property - Common way to access code that exists outside of class
+     * Can create public variable and access them that way, or you can use properties
+     * Properties ENCAPSULATES variables as fields
+     * GET Accessor (READ) - returns encapsulated variable values
+     * SET Accessor (WRITE) - allocates new values to property fields
+     * PROPERTY values use PascalCase
+     * SERIALIZEFIELD - makes private variables visible in inspector
+     * Awake() - called before Start(), preloads any variables or game state before game starts
+     * == is comparing, = is setting value, === is ALWAYS equal to
+     */
+
+    //Components
+    public bool AudioSFXSourceCreated { get; set; }
+
+    [field: SerializeField]
+    public AudioClip AudioClipOnStart { get; set; }
+
+    [field: SerializeField]
+    public  AudioClip AudioClipOnEnd { get; set; }
+
+    private AudioSource audioSource;
+
+    public FeatureUsage featureUsage = FeatureUsage.Once;
+
+    protected virtual void Awake()
+    {
+        MakeSFXAudioSource(); //calls method to create audio source
+    }
+
+    public void MakeSFXAudioSource()
+    { 
+        audioSource = GetComponent<AudioSource>();
+
+        //if equal to null, create it here
+        if (audioSource == null)
+        {
+            audioSource = gameObject.GetComponent<AudioSource>();
+        }
+
+        //whether null or not, still need to make sure that its true
+        //on awake, create audioscource
+        AudioSFXSourceCreated = true;
+    }
+}
